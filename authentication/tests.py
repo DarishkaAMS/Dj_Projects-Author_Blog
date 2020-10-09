66d3ecc6f0c5b9bf9e2fc4f86d25ea0686e83466
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-09 12:38:30 +0300

    Updated authentication/tests.py

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,21 +1,16 @@
-# from django.test import TestCase
-# from django.urls import reverse
-# # Create your tests here.
-# from authentication.models import UserManager, USER
-#
-#
-# class TestHello(TestCase):
-#     def setUp(self):
-#         user = UserManager.create_user(self, email="d@gmail.com")
-#         # user = self.model(email=email)
-#         user.set_password(password)
-#         user.save()
-#         # email.save()
-#         # password = UserManager(name="d@gmail.com")
-#         # password.save()
-#
-#     def test_hello_view(self):
-#         resp = self.client.get(reverse('authentication'))
-#         self.assertEqual(resp.status_code, 200)
-#         self.assertIn(self.article.title, resp.content.decode('utf8'))
-#
+from django.test import TestCase
+from django.urls import reverse
+# Create your tests here.
+from authentication.models import UserManager, USER
+
+
+class TestHello(TestCase):
+    def setUp(self):
+        self.user = UserManager.create_user(self, email="d@gmail.com", password="abc") #!!!!!!!
+        self.user.save()
+
+    def test_hello_view(self):
+        resp = self.client.get(reverse('authentication'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(self.article.title, resp.content.decode('utf8'))
+
